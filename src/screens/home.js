576cb9f4e1dc2df89a9529cb2750c8183016cc0a
antoineww <60444745+antoineww@users.noreply.github.com>
2020-01-29 18:17:06 -0400

    Updated home screen

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-const Home = () => null;
+const Home = () => (
+  <div class="App-header">
+    <h1>Welcome to the Trivia Challenge!</h1>
+    <img src="logo.svg" alt="logo" class="App-logo" />
+    <h2>
+      You will be presented with <br /> 10 True or False questions.
+    </h2>
+    <h2>Can you score 100%?</h2>
+    <button class="button">BEGIN</button>
+  </div>
+);
 
 export default Home;
