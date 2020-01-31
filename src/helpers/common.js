4d86f76491782cc1927eb4d7e1c424d93b2a356f
antoineww <60444745+antoineww@users.noreply.github.com>
2020-01-31 13:10:44 -0400

    Added question handling functions

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -1,5 +1,7 @@
+import { DEFAULT_QUESTION_WITH_ANSWER } from "./../resources/constants";
+
 export const getQuizProgess = (
-  questionCurrentIndex = 0,
+  questionCurrentIndex = -1,
   questionsWithAnswers = []
 ) => `${questionCurrentIndex + 1} of ${questionsWithAnswers.length}`;
 
@@ -7,3 +9,37 @@ export const getQuizScore = (questionsWithAnswers = []) =>
   `${
     questionsWithAnswers.filter(question => question.answer === true).length
   } / ${questionsWithAnswers.length}`;
+
+export const ensureCriticalQuestionProperties = (
+  possibleQuestionWithAnswer = {}
+) => {
+  const questionWithAnswer = { ...possibleQuestionWithAnswer };
+
+  questionWithAnswer.attempted_answer =
+    DEFAULT_QUESTION_WITH_ANSWER.attempted_answer;
+
+  if (
+    typeof questionWithAnswer.correct_answer !== "string" ||
+    !Array.isArray(questionWithAnswer.incorrect_answers)
+  ) {
+    questionWithAnswer.correct_answer =
+      DEFAULT_QUESTION_WITH_ANSWER.correct_answer;
+
+    questionWithAnswer.incorrect_answers =
+      DEFAULT_QUESTION_WITH_ANSWER.incorrect_answers;
+  }
+
+  return questionWithAnswer;
+};
+
+export const makeAQuizQuestion = (questionWithAnswer = {}) =>
+  ensureCriticalQuestionProperties({
+    ...DEFAULT_QUESTION_WITH_ANSWER,
+    ...questionWithAnswer
+  });
+
+export const answerQuizQuestion = (
+  questionCurrentIndex = 0,
+  questionsWithAnswers = [{}],
+  attempted_answer = null
+) => ({ ...questionsWithAnswers[questionCurrentIndex], attempted_answer });
--- src/resources/constants.js
+++ src/resources/constants.js
@@ -4,6 +4,15 @@ export const QUIZ_STAGES = {
   RESULTS: "RESULTS"
 };
 
+export const DEFAULT_QUESTION_WITH_ANSWER = {
+  category: "TOPIC: UNKNOWN",
+  question: "BAD QUESTION?",
+  correct_answer: "True",
+  incorrect_answers: ["False"],
+
+  attempted_answer: null
+};
+
 export const DEFAULT_STATE_QUIZ = {
   stage: QUIZ_STAGES.HOME,
   timeStart: null,
